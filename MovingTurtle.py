791684c3e7d342aa87f9fecb3a80f6b402ab981a
Jagrit Rattan <83203232+rjagrit@users.noreply.github.com>
2024-01-18 14:40:10 +0530

    Create MovingTurtle.py

---
+++ MovingTurtle.py
@@ -0,0 +1,13 @@
+# making a turtle screen
+import turtle
+s = turtle.getscreen()
+# t1= turtle.Turtle() -- 2nd turtle
+# my first triangle
+turtle.forward(90)
+turtle.right(120)
+turtle.forward(90)
+turtle.right(120)
+turtle.forward(90)
+
+# turtle.done() is very iumportant to freeze the Output Screen
+turtle.done()
